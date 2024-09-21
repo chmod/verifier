9aedf7f3962ee59bc9fa8838a753cffcd5ab2d4b
Panos Karampis <panagiotis.karampis@agillic.com>
2024-09-21 08:03:11 +0200

    init#4

--- src/main/java/dk/promofacie/wallet_verification/ReflectionConfig.java
+++ src/main/java/dk/promofacie/wallet_verification/ReflectionConfig.java
@@ -2,8 +2,9 @@ package dk.promofacie.wallet_verification;
 
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.ScheduledEvent;
 import net.dv8tion.jda.api.managers.AudioManager;
 
-@RegisterForReflection(targets = {Guild[].class, AudioManager[].class})
+@RegisterForReflection(targets = {Guild[].class, AudioManager[].class, ScheduledEvent[].class})
 public class ReflectionConfig {
 }
