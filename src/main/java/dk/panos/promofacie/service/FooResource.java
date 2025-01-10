3d08dea97ef968758b663692264f62f3eb53c6f0
Panagiotis Karampis <pkarampis@gmail.com>
2025-01-10 22:21:36 +0100

    new

--- src/main/java/dk/panos/promofacie/service/FooResource.java
+++ src/main/java/dk/panos/promofacie/service/FooResource.java
@@ -1,5 +1,6 @@
 package dk.panos.promofacie.service;
 
+import com.cronutils.utils.StringUtils;
 import dk.panos.promofacie.db.Wallet;
 import dk.panos.promofacie.radix.RadixClient;
 import dk.panos.promofacie.radix.model.GetTransactionsStreamResponse;
@@ -22,28 +23,25 @@ public class FooResource {
 
     @GET
     public Uni<Void> foo() {
-        Wallet w = new Wallet();
-        w.setAddress("account_rdx12y8ej0ydrf6eepnyq84ff4gqwev93y75x3lq90rzqs8yscdaf95lyw");
-        w.setDiscordId("1060005379662155886");
-        return w.persist().replaceWithVoid();
-//        Uni<GetTransactionsStreamResponse> transactions = radixClient.getTransactionsForAddress(new GetTransactionsStreamRequestNoLimit(List.of("account_rdx129wl7trqd7ttwfd2nx9tf3qgag6n2e5tjs3t9cfgslpp3yxh526e2u")));
-//        return transactions.onItem().transformToMulti(tx -> Multi.createFrom().iterable(tx.items()))
-//                .onItem()
-//                .transformToUni(item -> {
-//                    String sourceAddr = item.affectedGlobalEntities().stream().filter(str -> str.startsWith("account_rdx"))
-//                            .filter(str -> !str.equalsIgnoreCase("account_rdx129wl7trqd7ttwfd2nx9tf3qgag6n2e5tjs3t9cfgslpp3yxh526e2u"))
-//                            .findFirst().get();
-//                    if(item.message()==null) {
-//                        return Uni.createFrom().voidItem();
-//                    }
-//                    String discordId = item.message().content().value();
-//
-//                    Wallet wallet = new Wallet();
-//                    wallet.setDiscordId(discordId);
-//                    wallet.setAddress(sourceAddr);
-//                    return wallet.persist().replaceWithVoid();
-//                }).concatenate().collect().asList()
-//                .replaceWithVoid();
+
+        Uni<GetTransactionsStreamResponse> transactions = radixClient.getTransactionsForAddress(new GetTransactionsStreamRequestNoLimit(List.of("account_rdx129wl7trqd7ttwfd2nx9tf3qgag6n2e5tjs3t9cfgslpp3yxh526e2u")));
+        return transactions.onItem().transformToMulti(tx -> Multi.createFrom().iterable(tx.items()))
+                .onItem()
+                .transformToUni(item -> {
+                    String sourceAddr = item.affectedGlobalEntities().stream().filter(str -> str.startsWith("account_rdx"))
+                            .filter(str -> !str.equalsIgnoreCase("account_rdx129wl7trqd7ttwfd2nx9tf3qgag6n2e5tjs3t9cfgslpp3yxh526e2u"))
+                            .findFirst().get();
+                    if(item.message()==null) {
+                        return Uni.createFrom().voidItem();
+                    }
+                    String discordId = item.message().content().value();
+                    if(!StringUtils.isNumeric(discordId)) return Uni.createFrom().voidItem();
+                    Wallet wallet = new Wallet();
+                    wallet.setDiscordId(discordId);
+                    wallet.setAddress(sourceAddr);
+                    return wallet.persist().replaceWithVoid();
+                }).concatenate().collect().asList()
+                .replaceWithVoid();
     }
 
     public record GetTransactionsStreamRequestNoLimit(
