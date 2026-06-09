eb1cc061ea15e1bd5c9436c7a8021eb1e384aad5
Panagiotis Karampis <pkarampis@gmail.com>
2026-06-09 16:12:07 +0200

    kafka fix

--- src/main/java/dk/panos/promofacie/kafka/VerificationConsumer.java
+++ src/main/java/dk/panos/promofacie/kafka/VerificationConsumer.java
@@ -1,28 +1,28 @@
-package dk.panos.promofacie.kafka;
-
-import dk.panos.promofacie.db.Chain;
-import dk.panos.promofacie.db.Wallet;
-import dk.panos.promofacie.kafka.model.VerificationOutcome;
-import io.quarkus.panache.common.Parameters;
-import jakarta.transaction.Transactional;
-import jakarta.enterprise.context.ApplicationScoped;
-import org.eclipse.microprofile.reactive.messaging.Incoming;
-
-@ApplicationScoped
-public class VerificationConsumer {
-    @Incoming("verifier-rdx")
-    @Transactional
-    public void handleVerificationOutcome(VerificationOutcome verificationOutcome) {
-        if ("WALLET_VERIFICATION".equals(verificationOutcome.getPurpose()) && verificationOutcome.isOutcome()) {
-            Wallet wallet = Wallet.find("discordId = :discord_id and chain = :chain", Parameters.with("discord_id", verificationOutcome.getUserId()).and("chain", Chain.RADIX))
-                    .firstResult();
-            if (wallet == null) {
-                wallet = new Wallet();
-                wallet.setChain(Chain.RADIX);
-                wallet.setDiscordId(verificationOutcome.getUserId());
-            }
-            wallet.setAddress(verificationOutcome.getAddress());
-            wallet.persist();
-        }
-    }
-}
+//package dk.panos.promofacie.kafka;
+//
+//import dk.panos.promofacie.db.Chain;
+//import dk.panos.promofacie.db.Wallet;
+//import dk.panos.promofacie.kafka.model.VerificationOutcome;
+//import io.quarkus.panache.common.Parameters;
+//import jakarta.transaction.Transactional;
+//import jakarta.enterprise.context.ApplicationScoped;
+//import org.eclipse.microprofile.reactive.messaging.Incoming;
+//
+//@ApplicationScoped
+//public class VerificationConsumer {
+//    @Incoming("verifier-rdx")
+//    @Transactional
+//    public void handleVerificationOutcome(VerificationOutcome verificationOutcome) {
+//        if ("WALLET_VERIFICATION".equals(verificationOutcome.getPurpose()) && verificationOutcome.isOutcome()) {
+//            Wallet wallet = Wallet.find("discordId = :discord_id and chain = :chain", Parameters.with("discord_id", verificationOutcome.getUserId()).and("chain", Chain.RADIX))
+//                    .firstResult();
+//            if (wallet == null) {
+//                wallet = new Wallet();
+//                wallet.setChain(Chain.RADIX);
+//                wallet.setDiscordId(verificationOutcome.getUserId());
+//            }
+//            wallet.setAddress(verificationOutcome.getAddress());
+//            wallet.persist();
+//        }
+//    }
+//}
