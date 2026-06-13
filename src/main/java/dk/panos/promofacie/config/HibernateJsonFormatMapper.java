72274dbd947f87dee748b8eea6fed590cfdd57de
Panagiotis Karampis <pkarampis@gmail.com>
2026-06-13 08:13:18 +0200

    map add

---
+++ src/main/java/dk/panos/promofacie/config/HibernateJsonFormatMapper.java
@@ -0,0 +1,35 @@
+package dk.panos.promofacie.config;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import io.quarkus.hibernate.orm.PersistenceUnitExtension;
+import jakarta.inject.Singleton;
+import org.hibernate.type.descriptor.WrapperOptions;
+import org.hibernate.type.format.jackson.JacksonJsonFormatMapper;
+import org.hibernate.type.format.FormatMapper;
+
+@JsonFormat
+@PersistenceUnitExtension
+@Singleton
+public class HibernateJsonFormatMapper implements FormatMapper {
+
+    private final JacksonJsonFormatMapper delegate;
+
+    public HibernateJsonFormatMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        this.delegate = new JacksonJsonFormatMapper(mapper);
+    }
+
+
+    @Override
+    public <T> T fromString(CharSequence charSequence, org.hibernate.type.descriptor.java.JavaType<T> javaType, WrapperOptions wrapperOptions) {
+        return delegate.fromString(charSequence, javaType, wrapperOptions);
+    }
+
+    @Override
+    public <T> String toString(T value, org.hibernate.type.descriptor.java.JavaType<T> javaType, WrapperOptions wrapperOptions) {
+        return delegate.toString(value, javaType, wrapperOptions);
+    }
+}
