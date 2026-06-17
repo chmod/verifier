30d23a50712f77eccb1f0cc84381455ebc9b14b4
Panagiotis Karampis <pkarampis@gmail.com>
2026-06-17 06:55:28 +0200

    handle snapshot

--- src/main/java/dk/panos/promofacie/kafka/model/AmountPayload.java
+++ src/main/java/dk/panos/promofacie/kafka/model/AmountPayload.java
@@ -1,6 +1,7 @@
 package dk.panos.promofacie.kafka.model;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.Map;
 
@@ -9,10 +10,16 @@ import java.util.Map;
  * {@code traits} is omitted from JSON if the asset has no label-721 metadata.
  */
 public record AmountPayload(
+        @JsonProperty("unit")
         String unit,
+        @JsonProperty("policyId")
         String policyId,
+        @JsonProperty("assetName")
         String assetName,
+        @JsonProperty("quantity")
         String quantity,
         @JsonInclude(JsonInclude.Include.NON_NULL)
+        @JsonProperty("traits")
         Map<String, Object> traits
 ) {}
+
