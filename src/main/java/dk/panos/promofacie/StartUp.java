c6293a086b4dc4dbe970b08e21d93560446cf61d
Panagiotis Karampis <pkarampis@gmail.com>
2026-06-20 16:51:59 +0200

    policy tracking

--- src/main/java/dk/panos/promofacie/StartUp.java
+++ src/main/java/dk/panos/promofacie/StartUp.java
@@ -23,38 +23,38 @@ public class StartUp {
 
     @Transactional
     void onStart(@Observes StartupEvent ev) {
-        log.info("[StartUp] Cleaning pending_rule_evaluations table on startup");
-        dk.panos.promofacie.db.PendingRuleEvaluation.deleteAll();
-
-        String targetGuild = "979324485792567357";
-        String targetPolicy = "79371b11f083d53ba85ec2171f45bdd1d503d1b43f02c5f29dc3ab37";
-        String targetRoleId = "1515413108997493007";
-
-        log.info("[StartUp] Deleting existing GuildRoleRule for guild={}, policy={} if present", targetGuild, targetPolicy);
-        GuildRoleRule.delete("guildId = ?1 and policyId = ?2 and roleId = ?3", targetGuild, targetPolicy, targetRoleId);
-
-        log.info("[StartUp] Creating new GuildRoleRule for guild={}, policy={}", targetGuild, targetPolicy);
-        GuildRoleRule rule = new GuildRoleRule();
-        rule.guildId = targetGuild;
-        rule.policyId = targetPolicy;
-        rule.roleId = targetRoleId;
-        rule.minQuantity = 1L;
-        
-        ruleRegistrationService.registerRuleAndSync(rule);
-
-        String eikoTargetPolicy = "0a109e4c024759806827258f6e3f316fe94584ecd1f85eb18bbce0d9";
-        String eikoTargetRoleId = "1517558406695620659";
-
-        log.info("[StartUp] Deleting existing GuildRoleRule for guild={}, policy={} if present", targetGuild, eikoTargetPolicy);
-        GuildRoleRule.delete("guildId = ?1 and policyId = ?2 and roleId = ?3", targetGuild, eikoTargetPolicy, eikoTargetRoleId);
-
-        log.info("[StartUp] Creating new GuildRoleRule for guild={}, policy={}", targetGuild, eikoTargetPolicy);
-        GuildRoleRule eikoRule = new GuildRoleRule();
-        eikoRule.guildId = targetGuild;
-        eikoRule.policyId = eikoTargetPolicy;
-        eikoRule.roleId = eikoTargetRoleId;
-        eikoRule.minQuantity = 1L;
-        
-        ruleRegistrationService.registerRuleAndSync(eikoRule);
+//        log.info("[StartUp] Cleaning pending_rule_evaluations table on startup");
+//        dk.panos.promofacie.db.PendingRuleEvaluation.deleteAll();
+//
+//        String targetGuild = "979324485792567357";
+//        String targetPolicy = "79371b11f083d53ba85ec2171f45bdd1d503d1b43f02c5f29dc3ab37";
+//        String targetRoleId = "1515413108997493007";
+//
+//        log.info("[StartUp] Deleting existing GuildRoleRule for guild={}, policy={} if present", targetGuild, targetPolicy);
+//        GuildRoleRule.delete("guildId = ?1 and policyId = ?2 and roleId = ?3", targetGuild, targetPolicy, targetRoleId);
+//
+//        log.info("[StartUp] Creating new GuildRoleRule for guild={}, policy={}", targetGuild, targetPolicy);
+//        GuildRoleRule rule = new GuildRoleRule();
+//        rule.guildId = targetGuild;
+//        rule.policyId = targetPolicy;
+//        rule.roleId = targetRoleId;
+//        rule.minQuantity = 1L;
+//
+//        ruleRegistrationService.registerRuleAndSync(rule);
+//
+//        String eikoTargetPolicy = "0a109e4c024759806827258f6e3f316fe94584ecd1f85eb18bbce0d9";
+//        String eikoTargetRoleId = "1517558406695620659";
+//
+//        log.info("[StartUp] Deleting existing GuildRoleRule for guild={}, policy={} if present", targetGuild, eikoTargetPolicy);
+//        GuildRoleRule.delete("guildId = ?1 and policyId = ?2 and roleId = ?3", targetGuild, eikoTargetPolicy, eikoTargetRoleId);
+//
+//        log.info("[StartUp] Creating new GuildRoleRule for guild={}, policy={}", targetGuild, eikoTargetPolicy);
+//        GuildRoleRule eikoRule = new GuildRoleRule();
+//        eikoRule.guildId = targetGuild;
+//        eikoRule.policyId = eikoTargetPolicy;
+//        eikoRule.roleId = eikoTargetRoleId;
+//        eikoRule.minQuantity = 1L;
+//
+//        ruleRegistrationService.registerRuleAndSync(eikoRule);
     }
 }
