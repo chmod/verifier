e4971649731e2a10bafa25192e0b515602cb3271
Panagiotis Karampis <pkarampis@gmail.com>
2026-06-27 21:52:25 +0200

    tx kafka

--- src/main/java/dk/panos/promofacie/kafka/TransactionMessageConsumer.java
+++ src/main/java/dk/panos/promofacie/kafka/TransactionMessageConsumer.java
@@ -1,7 +1,9 @@
 package dk.panos.promofacie.kafka;
 
 import dk.panos.promofacie.kafka.model.TransactionMessage;
+import dk.panos.promofacie.service.DiscordNotificationService;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +11,9 @@ import org.slf4j.LoggerFactory;
 @ApplicationScoped
 public class TransactionMessageConsumer {
     private static final Logger log = LoggerFactory.getLogger(TransactionMessageConsumer.class);
+
+    @Inject
+    DiscordNotificationService notificationService;
 
     @Incoming("transactions")
     public void consumeTransaction(TransactionMessage message) {
@@ -18,5 +23,7 @@ public class TransactionMessageConsumer {
         }
         log.info("[TransactionConsumer] Consumed transaction: quantity={}, name={}, url={}",
                 message.quantity(), message.name(), message.url());
+        
+        notificationService.sendTransactionNotification(message);
     }
 }
---
+++ src/main/java/dk/panos/promofacie/service/DiscordNotificationService.java
@@ -0,0 +1,53 @@
+package dk.panos.promofacie.service;
+
+import dk.panos.promofacie.kafka.model.TransactionMessage;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@ApplicationScoped
+public class DiscordNotificationService {
+    private static final Logger log = LoggerFactory.getLogger(DiscordNotificationService.class);
+
+    private static final String GUILD_ID = "979324485792567357";
+    private static final String CHANNEL_ID = "1516371490755579914";
+
+    private final JDA jda;
+
+    @Inject
+    public DiscordNotificationService(JDA jda) {
+        this.jda = jda;
+    }
+
+    public void sendTransactionNotification(TransactionMessage message) {
+        try {
+            Guild guild = jda.getGuildById(GUILD_ID);
+            if (guild == null) {
+                log.error("Could not find Discord Guild with ID: {}", GUILD_ID);
+                return;
+            }
+
+            TextChannel channel = guild.getTextChannelById(CHANNEL_ID);
+            if (channel == null) {
+                log.error("Could not find Text Channel with ID: {} in Guild: {}", CHANNEL_ID, guild.getName());
+                return;
+            }
+
+            String formattedMessage = String.format("**New Transaction Detected!**\n" +
+                    "**Asset:** %s\n" +
+                    "**Quantity:** %s\n" +
+                    "**View:** %s", message.name(), message.quantity(), message.url());
+
+            channel.sendMessage(formattedMessage).queue(
+                success -> log.info("Successfully sent transaction notification to Discord channel: {}", CHANNEL_ID),
+                failure -> log.error("Failed to send message to Discord channel: {}", CHANNEL_ID, failure)
+            );
+        } catch (Exception e) {
+            log.error("Failed to process transaction notification to Discord", e);
+        }
+    }
+}
